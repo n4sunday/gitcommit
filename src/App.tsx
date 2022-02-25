a551f9d31f05c9540643b3fd820806e6f9601720
Sunday <dev.nattapon.s@gmail.com>
2022-02-25 18:10:44 +0700

    ✨ :feat: add new icon commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,7 @@ import React, { useState } from "react";
 
 import CardIcon from "./components/Card/CardIcon";
 import MessageCard from "./components/Message/MessageCard";
-
-const commit = [
-  {
-    name: ":build:",
-    icon: "👷",
-    color: "bg-build",
-    description: "Build related changes (eg: npm related/ adding dependencies)",
-  },
-  {
-    name: ":chore:",
-    icon: "🔧",
-    color: "bg-chore",
-    description: "A code change that external user won't see.",
-  },
-  {
-    name: ":feat:",
-    icon: "✨",
-    color: "bg-feat",
-    description: "A new feature.",
-  },
-  {
-    name: ":docs:",
-    icon: "📝",
-    color: "bg-docs",
-    description: "Documentation related changes.",
-  },
-  {
-    name: ":perf:",
-    icon: "⚡️",
-    color: "bg-perf",
-    description: "A code that improves performance.",
-  },
-  {
-    name: ":refactor:",
-    icon: "♻️",
-    color: "bg-refactor",
-    description: "A code that neither fix bug nor adds a feature.",
-  },
-  {
-    name: ":fix:",
-    icon: "🚑️",
-    color: "bg-fix",
-    description: "A bug fix.",
-  },
-  {
-    name: ":style:",
-    icon: "💄",
-    color: "bg-style",
-    description: "A code that is related to styling.",
-  },
-  {
-    name: ":test:",
-    icon: "✅",
-    color: "bg-test",
-    description: "Adding new test or making changes to existing test.",
-  },
-];
+import { commit } from "./constants/commit";
 
 function App() {
   const [message, setMessage] = useState("");
--- theme.presets.js
+++ theme.presets.js
@@ -2,47 +2,42 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bgapp: '#0f0f13',
-        app: '#222429',
-        apphover: '#141419',
+        bgapp: "#0f0f13",
+        app: "#222429",
+        apphover: "#141419",
         copyfrom: "#e84f3d",
         copyto: "#fb9c51",
         refactor: "#77e856",
         build: "#64b5f6",
         fix: "#ffc400",
         chore: "#ffc400",
-        feat: "#ffe55f",
-        docs: "#ffe55f",
+        feat: "#fb584a",
+        docs: "#7f39fb",
         perf: "#40c4ff",
         style: "#80deea",
-        test: "#77e856"
+        test: "#77e856",
+        del: "#ff9d44",
+        moon: "#00a9f0",
       },
-      backgroundImage: (theme) => ({
-      }),
-      backgroundSize: (theme) => ({
-
-      }),
+      backgroundImage: (theme) => ({}),
+      backgroundSize: (theme) => ({}),
       height: (theme) => ({
-        '10-percent': '10.5%',
-        'screen-68': '68vh',
-        'screen-80': '80vh',
-        'screen-90': '90vh',
-        'screen-72': '72vh',
-        'screen-60': '60vh',
-        'screen-10': '10vh',
-        'px-642': '642px',
-        'px-550': '550px',
+        "10-percent": "10.5%",
+        "screen-68": "68vh",
+        "screen-80": "80vh",
+        "screen-90": "90vh",
+        "screen-72": "72vh",
+        "screen-60": "60vh",
+        "screen-10": "10vh",
+        "px-642": "642px",
+        "px-550": "550px",
       }),
-      width: (theme) => ({
-
-      }),
+      width: (theme) => ({}),
       fontSize: (theme) => ({
-        xxs: '0.5rem',
+        xxs: "0.5rem",
       }),
-      padding: (theme) => ({
-
-      }),
+      padding: (theme) => ({}),
     },
   },
   plugins: [],
-}
+};
